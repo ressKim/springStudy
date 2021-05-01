b67630e2fb69b75f5ae92a6b1d9b1ec1a7515a5a
ressKim <sh21dlat@gmail.com>
2021-05-01 19:16:07 +0900

    mvc패턴 적용해보기

---
+++ src/main/java/com/example/hellospring/web/servletmvc/MvcMemberListServlet.java
@@ -0,0 +1,32 @@
+package com.example.hellospring.web.servletmvc;
+
+import com.example.hellospring.domain.member.Member;
+import com.example.hellospring.domain.member.MemberRepository;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "mvcMemberListServlet", urlPatterns = "/servlet-mvc/members")
+public class MvcMemberListServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        List<Member> all = memberRepository.findAll();
+
+        request.setAttribute("members", all);
+        String viewPath = "/WEB-INF/views/members.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+
+    }
+}
