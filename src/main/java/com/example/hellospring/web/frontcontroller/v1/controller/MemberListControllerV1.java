c16e19e875a70213c3e4a7d233de86a83cc31105
ressKim <sh21dlat@gmail.com>
2021-05-02 13:46:04 +0900

    mvc 프레임워크 만들기 시작 - front-controller 도입

---
+++ src/main/java/com/example/hellospring/web/frontcontroller/v1/controller/MemberListControllerV1.java
@@ -0,0 +1,30 @@
+package com.example.hellospring.web.frontcontroller.v1.controller;
+
+import com.example.hellospring.domain.member.Member;
+import com.example.hellospring.domain.member.MemberRepository;
+import com.example.hellospring.web.frontcontroller.v1.ControllerV1;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class MemberListControllerV1 implements ControllerV1 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        List<Member> all = memberRepository.findAll();
+
+        request.setAttribute("members", all);
+        String viewPath = "/WEB-INF/views/members.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+
+
+    }
+}
