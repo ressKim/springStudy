2aead6a5a9a2e8e57aaaa6c07b1165b033181474
ressKim <sh21dlat@gmail.com>
2021-05-21 01:26:09 +0900

    spring mvc 로 mvc 공부

---
+++ src/main/java/com/example/hellospring/web/springmvc/v1/SpringMemberListControllerV1.java
@@ -0,0 +1,28 @@
+package com.example.hellospring.web.springmvc.v1;
+
+import com.example.hellospring.domain.member.Member;
+import com.example.hellospring.domain.member.MemberRepository;
+import com.example.hellospring.web.frontcontroller.ModelView;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class SpringMemberListControllerV1 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/springmvc/v1/members")
+    public ModelAndView process() {
+
+        List<Member> members = memberRepository.findAll();
+
+        ModelAndView mv = new ModelAndView("members");
+        mv.addObject("members", members);
+        return mv;
+    }
+
+}
